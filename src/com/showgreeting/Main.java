495372f031a75b9025802955d795f792a57a98e7
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-04 16:31:10 +0700

    Add all file

---
+++ src/com/showgreeting/Main.java
@@ -0,0 +1,13 @@
+package com.showgreeting;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello " + name);
+    }
+}
